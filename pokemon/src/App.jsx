895228638a5c405e5d2f3f2d0065b960a5a8a56e
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-12-02 19:49:29 +0800

    Add page and react router

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -1,6 +1,19 @@
-import Home from "./components/Home";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import DigimonList from "./pages/DigimonList";
+import Home from "./pages/Home";
+import PokemonList from "./pages/PokemonList";
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemon" element={<PokemonList />} />
+          <Route path="/digimon" element={<DigimonList />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
