b29fb5c6fe983d2df4075067881b5c3cd461b019
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-12-03 11:56:47 +0800

    Add custom background gradient color

--- pokemon/tailwind.config.js
+++ pokemon/tailwind.config.js
@@ -3,7 +3,13 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-to-l-pokemon': 'linear-gradient(270deg, #D3EEF4ff, #D8EEEDff, #DDEEE5ff, #E2EEDEff, #E7EED7ff, #ECEECFff, #F1EEC8ff)',
+        'gradient-to-l-home': 'linear-gradient(270deg, #B597F6ff, #B09FF4ff, #ABA7F2ff, #A6AFF0ff, #A0B6EEff, #9BBEECff, #96C6EAff)',
+        'gradient-to-l-digimon': 'linear-gradient(90deg, #F0EAFCff, #D8E5FAff, #C0E1F9ff, #A9DCF7ff, #91D7F5ff, #79D3F4ff, #61CEF2ff)',
+      },
+    },
   },
   plugins: [],
 }
