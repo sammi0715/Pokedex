3bd528455cbe745c8da52db3ba09ccd67b1bb207
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-12-03 22:51:16 +0800

    Create pokemon and digimon color scheme

---
+++ pokemon/src/utils/color-scheme.js
@@ -0,0 +1,29 @@
+//Pokemon palette
+export const typeColors = {
+    normal: "#c2c2a1",
+    fire: "#fb6c6c",
+    water: "#609fb5",
+    electric: "#ffd86f",
+    grass: "#48d0b0",
+    ice: "#7fccec",
+    fighting: "#d6b591",
+    poison: "#7c538c",
+    ground: "#b1736c",
+    flying: "#bab0d5",
+    psychic: "#9b7fa6",
+    bug: "#c3ce75",
+    rock: "#a6aab6",
+    ghost: "#7c538c",
+    dragon: "#f9be00",
+    dark: "#333333",
+    steel: "#ccccde",
+    fairy: "#f469a9",
+};
+//Digimon palette
+export const attributeColors = {
+    Vaccine: "#48D1CC",
+    Data: "#FFD700",
+    Virus: "#FF4500",
+    Free: "#9370DB",
+    None: "#C0C0C0",
+};
