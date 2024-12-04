8873b276b0e5576662811da9763eef5d2df3bda6
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-12-04 20:24:40 +0800

    Add animation and key frames

--- pokemon/tailwind.config.js
+++ pokemon/tailwind.config.js
@@ -9,6 +9,16 @@ export default {
         'gradient-to-l-home': 'linear-gradient(270deg, #B597F6ff, #B09FF4ff, #ABA7F2ff, #A6AFF0ff, #A0B6EEff, #9BBEECff, #96C6EAff)',
         'gradient-to-l-digimon': 'linear-gradient(90deg, #F0EAFCff, #D8E5FAff, #C0E1F9ff, #A9DCF7ff, #91D7F5ff, #79D3F4ff, #61CEF2ff)',
       },
+      animation: {
+        "spin-color-change": "spin 1s linear infinite, color-change 1s ease-in-out infinite",
+      },
+
+      keyframes: {
+        'color-change': {
+          "0%, 100%": { color: "#F34F44" },
+          "50%": { color: "#368AFF" },
+        },
+      },
     },
   },
   plugins: [],
