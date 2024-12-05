5fe0aeb40dc059477f57e9f5906597a550ca73e4
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-12-05 14:55:54 +0800

    Change gradient color and add marquee animation

--- pokemon/tailwind.config.js
+++ pokemon/tailwind.config.js
@@ -6,21 +6,39 @@ export default {
     extend: {
       backgroundImage: {
         'gradient-to-l-pokemon': 'linear-gradient(270deg, #D3EEF4ff, #D8EEEDff, #DDEEE5ff, #E2EEDEff, #E7EED7ff, #ECEECFff, #F1EEC8ff)',
-        'gradient-to-l-home': 'linear-gradient(270deg, #B597F6ff, #B09FF4ff, #ABA7F2ff, #A6AFF0ff, #A0B6EEff, #9BBEECff, #96C6EAff)',
+        'gradient-to-l-home': 'linear-gradient(135deg, hsla(197, 85%, 79%, 1) 0%, hsla(55, 44%, 95%, 1) 52%, hsla(27, 95%, 84%, 1) 100%)',
         'gradient-to-l-digimon': 'linear-gradient(90deg, #F0EAFCff, #D8E5FAff, #C0E1F9ff, #A9DCF7ff, #91D7F5ff, #79D3F4ff, #61CEF2ff)',
+        'back': "url('/src/assets/pokemon.png')",
+        'gradient': "url('/src/assets/pokemon-gradient-flash.png')",
       },
       animation: {
         "spin-color-change": "spin 1s linear infinite, color-change 1s ease-in-out infinite",
+        marquee: "marquee 15s linear infinite",
+        "marquee-reverse": "marquee-reverse 15s linear infinite",
       },
-
       keyframes: {
         'color-change': {
           "0%, 100%": { color: "#F34F44" },
           "50%": { color: "#368AFF" },
         },
+        'marquee': {
+          from: {
+            transform: "translateX(0%)",
+          },
+          to: {
+            transform: "translateX(-100%)",
+          },
+        },
+        'marquee-reverse': {
+          from: {
+            transform: "translateX(-100%)",
+          },
+          to: {
+            transform: "translateX(0%)",
+          },
+        },
       },
     },
   },
   plugins: [],
-}
-
+};
