4e6a1082fb327b27f10bb2935890787109ea752a
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-12-05 22:12:29 +0800

    Add Navbar component

--- pokemon/src/App.jsx
+++ pokemon/src/App.jsx
@@ -1,18 +1,18 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import DigimonList from "./pages/DigimonList";
 import Home from "./pages/Home";
+import Navbar from "./pages/Navbar";
 import PokemonList from "./pages/PokemonList";
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pokemon" element={<PokemonList />} />
-          <Route path="/digimon" element={<DigimonList />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon" element={<PokemonList />} />
+        <Route path="/digimon" element={<DigimonList />} />
+      </Routes>
+    </Router>
   );
 }
 
