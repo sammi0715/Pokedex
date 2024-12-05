1af43d17b9e7efaf8c5f1638f7e9509a2ac3d1ed
sammi0715 <167386985+sammi0715@users.noreply.github.com>
2024-12-05 22:28:59 +0800

    Change logo to image and manage error conditions

--- pokemon/src/pages/DigimonList.jsx
+++ pokemon/src/pages/DigimonList.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import digiLogo from "../assets/digi-logo.png";
 import DigimonDetailCard from "../components/DigimonDetailCard";
+import ErrorMessage from "../components/ErrorMessage";
 import Loading from "../components/Loading";
 import Pagination from "../components/Pagination";
 import { fetchDigimonDetails, fetchDigimonList } from "../utils/api";
 import { attributeColors } from "../utils/color-scheme";
+
 function DigimonList() {
   const ITEMS_PER_PAGE = 20;
   const TOTAL_DIGIMON = 1460;
@@ -19,18 +22,32 @@ function DigimonList() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await fetchDigimonList(currentPage, ITEMS_PER_PAGE);
-        setDigimon(data.content);
-        setLoading(true);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
+  const fetchData = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const data = await fetchDigimonList(currentPage, ITEMS_PER_PAGE);
+      setDigimon(data.content);
+    } catch (err) {
+      if (err.response && err.response.status === 404) {
+        setError({ message: "Data not found, please try again later!", type: "server" });
+      } else if (err.message === "Network Error") {
+        setError({
+          message: "Network error, please check your internet connection!",
+          type: "network",
+        });
+      } else {
+        setError({
+          message: "An unknown error occurred, please try again later!",
+          type: "unknown",
+        });
       }
-    };
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
   }, [currentPage]);
 
@@ -46,7 +63,7 @@ function DigimonList() {
         const detailsMap = Object.fromEntries(details.map((d) => [d.id, d.attributes]));
         setDigimonDetails(detailsMap);
       } catch (err) {
-        console.error(err);
+        console.error("Failed to fetch Digimon details:", err);
       }
     };
     if (digimon.length > 0) {
@@ -60,8 +77,12 @@ function DigimonList() {
     }
   };
   const handleDigimonClick = (id) => {
-    setSelectedDigimonId(id);
-    setIsModalOpen(true);
+    if (digimonDetails[id]) {
+      setSelectedDigimonId(id);
+      setIsModalOpen(true);
+    } else {
+      alert("Failed to load Digimon details. Please try again later.");
+    }
   };
 
   const closeModal = () => {
@@ -69,21 +90,20 @@ function DigimonList() {
     setIsModalOpen(false);
   };
 
-  if (error) {
-    return <div>錯誤：{error}</div>;
-  }
   return (
     <div className="min-h-screen bg-gradient-to-l-digimon p-4">
-      <header className="relative flex items-center py-4 px-6">
-        <Link to="/" className="z-10">
-          <FaHome className="w-8 h-8 cursor-pointer" />
-        </Link>
-        <h1 className="flex absolute inset-0 text-4xl text-center items-center justify-center">
-          Digimon
-        </h1>
+      <header className="relative flex items-center justify-center py-4 px-6">
+        <div className="w-12 h-12 md:w-14 md:h-14 bg-white bg-opacity-50 absolute left-6  items-center flex justify-center rounded-full">
+          <Link to="/" className="">
+            <FaHome className="w-8 h-8 md:w-10 md:h-10  text-orange-400 hover:text-orange-600 cursor-pointer" />
+          </Link>
+        </div>
+        <img src={digiLogo} className="w-[150px] md:w-[300px]" alt="Digimon Logo" />
       </header>
-      {loading ? (
-        <Loading itemsPerPage={ITEMS_PER_PAGE} />
+      {error ? (
+        <ErrorMessage message={`Error：${error.message}`} onRetry={fetchData} type={error.type} />
+      ) : loading ? (
+        <Loading type="list" message="Gotta catch 'em all..." itemsPerPage={ITEMS_PER_PAGE} />
       ) : (
         <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4 md:p-12">
           {digimon.map((d) => {
@@ -92,7 +112,7 @@ function DigimonList() {
             return (
               <li
                 key={d.id}
-                className="bg-white bg-opacity-50 shadow-md flex flex-col p-4 mb-4 rounded-xl"
+                className="bg-white bg-opacity-50 shadow-md flex flex-col p-4 mb-4 rounded-xl transform transition duration-300 hover:scale-105 hover:shadow-lg"
                 onClick={() => handleDigimonClick(d.id)}
               >
                 <p className="text-gray-400 text-right">#{d.id}</p>
